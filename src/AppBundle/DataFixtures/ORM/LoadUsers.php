8edbb8f7347487fe1ca098e5ef2180fae83cd389
Dan Richards <dan.m.richards@gmail.com>
2016-10-22 22:14:17 +0100

    Added fixtures for new entities. Updated user entity fixture to work with new entities

--- src/AppBundle/DataFixtures/ORM/LoadUsers.php
+++ src/AppBundle/DataFixtures/ORM/LoadUsers.php
@@ -2,12 +2,12 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Common\DataFixtures\ORM;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\User;
 
-class LoadUsers implements FixtureInterface
+class LoadUsers extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -16,13 +16,21 @@ class LoadUsers implements FixtureInterface
         $user1->setBio('He is a cool guy');
         $user1->setEmail('john@mava.info');
         $manager->persist($user1);
+        $this->addReference('user-john', $user1);
 
         $user2 = new User();
         $user2->setName('Jack');
         $user2->setBio('He is a cool guy too');
         $user2->setEmail('jack@mava.info');
         $manager->persist($user2);
+        $this->addReference('user-jack', $user2);
 
         $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        // The order in which the fixtures will be loaded.
+        return 30;
     }
 }
