989c21f2d2ad69a3d57f214c51417bbbfe88ecc0
Dan Richards <dan.m.richards@gmail.com>
2016-10-22 23:22:33 +0100

    Configured a test database name in parameters yaml. Updated test config to use test database name. Updated codeception config to run tests against the test database and restore a dump before each test is run.

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,143 @@
-/* Replace this file with actual dump of your database */
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.12)
+# Database: mava_test
+# Generation Time: 2016-10-22 22:11:15 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Recreate the database.
+DROP DATABASE IF EXISTS `mava_test`;
+CREATE DATABASE `mava_test`;
+
+-- Ensure we use the test database
+USE `mava_test`;
+
+# Dump of table project
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `project`;
+
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `workspace_id` int(11) DEFAULT NULL,
+  `title` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `description` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `due_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_project_idx` (`workspace_id`),
+  CONSTRAINT `FK_2FB3D0EE82D40A1F` FOREIGN KEY (`workspace_id`) REFERENCES `workspace` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+
+INSERT INTO `project` (`id`, `workspace_id`, `title`, `description`, `due_date`)
+VALUES
+	(1,1,'Symfony book','Some descriptions for Symfony book project','2014-10-20 00:00:00');
+
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table task
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `task`;
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `description` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `due_date` datetime NOT NULL,
+  `attachment` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_task_1_idx_idx` (`project_id`),
+  KEY `fk_task_2_idx_idx` (`user_id`),
+  CONSTRAINT `FK_527EDB25166D1F9C` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
+  CONSTRAINT `FK_527EDB25A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+
+INSERT INTO `task` (`id`, `project_id`, `user_id`, `title`, `description`, `due_date`, `attachment`)
+VALUES
+	(1,1,1,'Writing chapter 1','Descriptions for writing ch1','2014-10-14 00:00:00',NULL),
+	(2,1,2,'Reviewing chapter 1','Descriptions for reviewing ch1','2014-10-16 00:00:00',NULL),
+	(3,1,2,'Editing chapter 1','Descriptions for editing ch1','2014-10-18 00:00:00',NULL);
+
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `bio` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+
+INSERT INTO `user` (`id`, `name`, `bio`, `email`)
+VALUES
+	(1,'John','He is a cool guy','john@mava.info'),
+	(2,'Jack','He is a cool guy too','jack@mava.info');
+
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table workspace
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `workspace`;
+
+CREATE TABLE `workspace` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `description` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+LOCK TABLES `workspace` WRITE;
+/*!40000 ALTER TABLE `workspace` DISABLE KEYS */;
+
+INSERT INTO `workspace` (`id`, `name`, `description`)
+VALUES
+	(1,'Writing','Info for writing workspace');
+
+/*!40000 ALTER TABLE `workspace` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
