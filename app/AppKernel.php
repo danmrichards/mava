979857c99341f38aa8efb0bc3e6ad203272c3bfd
Dan Richards <dan.m.richards@gmail.com>
2016-11-03 21:25:30 +0000

    Installed and configured Sonata

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,11 @@ class AppKernel extends Kernel
             new MyBundle\MyBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
