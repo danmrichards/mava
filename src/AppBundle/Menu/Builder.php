d4f846371f67491f2c1a9b2b2450cc9c7a02f533
Dan Richards <dan.m.richards@gmail.com>
2016-11-08 01:00:34 +0000

    Added override for FOSuserBundle templates. Fixed css/js compiling issue. Added sidebar menu.

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -2,10 +2,24 @@
 namespace AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class Builder extends ContainerBuilder
+class Builder implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
+    /**
+     * Build the top menu.
+     *
+     * @param FactoryInterface $factory
+     *   Menu building factory.
+     * @param array $options
+     *   Array of menu options.
+     *
+     * @return \Knp\Menu\ItemInterface
+     *   The constructed menu.
+     */
     public function topMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
@@ -29,9 +43,62 @@ class Builder extends ContainerBuilder
         ));
 
         // Create a dropdown header.
-        $dropdown2->addChild('Edit Profile', array('route' => 'sonata_user_profile_edit'));
-        $dropdown2->addChild('Change Password', array('route' => 'sonata_user_change_password'));
-        $dropdown2->addChild('Logout', array('route' => 'sonata_user_security_logout'));
+        $dropdown2->addChild('Edit Profile', array('route' => 'fos_user_profile_edit'));
+        $dropdown2->addChild('Change Password', array('route' => 'fos_user_change_password'));
+        $dropdown2->addChild('Logout', array('route' => 'fos_user_security_logout'));
+
+        return $menu;
+    }
+
+    /**
+     * Build the sidebar menu.
+     *
+     * @param FactoryInterface $factory
+     *   Menu building factory.
+     * @param array $options
+     *   Array of menu options.
+     *
+     * @return \Knp\Menu\ItemInterface
+     *   The constructed menu.
+     */
+    public function sideMenu(FactoryInterface $factory, array $options)
+    {
+        // Menu will be a navbar menu anchored to right
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('id', 'side-menu');
+        $menu->setChildrenAttribute('class', 'nav');
+
+        // Create a dropdown with a caret
+        $menu->addChild('Dashboard', array(
+            'icon' => 'dashboard',
+            'route' => 'dashboard_index'
+        ));
+
+        // Create a dropdown header
+        $dropdown = $menu->addChild('Project', array(
+            'icon'  => 'tasks',
+            'dropdown' => true,
+            'caret' => true,
+        ));
+
+        $dropdown->setChildrenAttribute('class', 'nav nav-second-level');
+
+        // Get the list of projects.
+        $em = $this->container->get('doctrine')->getManager();
+        $projects = $em->getRepository('AppBundle:Project')->findAll();
+
+        // Add a link for each project.
+        foreach ($projects as $project) {
+            $dropdown->addChild($project->getTitle(), array(
+                'route' => 'project_show',
+                'routeParameters' => array('id' => $project->getId()),
+            ));
+        }
+
+        $menu->addChild('Team', array(
+            'icon' => 'thumbs-up',
+            'uri'  => '#',
+        ));
 
         return $menu;
     }
